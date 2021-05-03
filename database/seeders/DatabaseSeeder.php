c8c14881b6a4ced38dc497b73a85654db8a4fbc8
Darylabrador <abrador.daryl@gmail.com>
2021-05-03 14:30:15 +0400

    Adding : Login / logout / email verify / check token

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = \Faker\Factory::create("fr_FR");
+
+        $this->call([
+            RoleSeeder::class,
+        ]);
+
+
+        $mailArray = ["admin@simplonapp.re", "formateur@simplonapp.re", "apprenant@simplonapp.re"];
+
+        for ($i=1; $i < 4; $i++) { 
+            $user = new User();
+            $user->name         = $faker->firstName();
+            $user->surname      = $faker->lastName();
+            $user->email        = $mailArray[$i - 1];
+            $user->password     = Hash::make('password');
+            $user->verified_at  = now();
+            $user->role_id      = $i;
+            $user->save(); 
+        }
     }
 }
