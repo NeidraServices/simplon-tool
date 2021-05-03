c0918c9e2e34875859be18debd1a2e518a8893cd
SharlyneEvynna <56205156+fredy-ID@users.noreply.github.com>
2021-05-03 14:37:55 +0400

    migration : projet

---
+++ database/migrations/2021_05_03_103047_create_deliver_projet.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliverProjet extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliver_projet', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->string('image');
+            $table->datetime('deadline');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliver_projet');
+    }
+}
