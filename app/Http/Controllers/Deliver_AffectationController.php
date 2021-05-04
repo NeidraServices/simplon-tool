afeed066ea206494821c7a19bff1765494745088
florent etheve <fetheve2@gmail.com>
2021-05-04 11:46:57 +0400

    affectation apprenants

---
+++ app/Http/Controllers/Deliver_AffectationController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Deliver_UsersModel;
+use App\Models\Deliver_ProjetModel;
+
+class Deliver_AffectationController extends Controller
+{
+    //
+    public function affecter(Request $req){
+
+        $data=$req->all();
+        $user=Deliver_UsersModel::find($data["user_id"]);
+        $projet=Deliver_ProjetModel::find($data["projet_id"]);
+
+        //si l'apprenant est déjà affecté au projet
+        $affectation=Deliver_ProjetModel::with("users")->whereHas("users",function($users) use($data){
+            $users->where("user_id",$data["user_id"])->where("projet_id",$data["projet_id"]);
+        })->get();
+       
+
+        if(sizeof($affectation)==0){
+
+        $user->projets()->attach($data["user_id"],["projet_id"=>$data["projet_id"]]);
+
+        }else{
+            return response()->json(["error"=>"cette apprenant est d&eacute;j&agrave; affect&eacute; au projet ".$projet["titre"]]);
+        }
+    }
+
+    public function supprimerApprenant(Request $req){
+        $data=$req->all();
+        $user=Deliver_UsersModel::find($data["user_id"]);
+        $projet=Deliver_ProjetModel::find($data["projet_id"]);
+
+        $user->projets()->detach($projet["id"]);
+
+    }
+}
