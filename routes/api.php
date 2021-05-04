9555a81cf5c44f1db0356e7cc1bce73928690bbc
florent etheve <fetheve2@gmail.com>
2021-05-04 08:06:27 +0400

    add route projet deliver

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Deliver_ProjetController;
+use App\Http\Controllers\Deliver_CommentairesController;
+use App\Http\Controllers\Deliver_CompetenceController;
+use App\Http\Controllers\Deliver_MediaController;
+use App\Http\Controllers\Deliver_TagController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,3 +30,15 @@ Route::post('/connexion', [AuthController::class, 'connexion'])->name('api.conne
 Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('api.logout');
 Route::post('/email/verification', [AuthController::class, 'verifymail'])->name('api.verify.email');
 Route::post('/verify', [AuthController::class, 'verifyToken'])->middleware(['auth:api'])->name('api.verify.token');
+
+
+
+Route::prefix('deliver')->group(function () {
+
+Route::resources("/projet",[Deliver_ProjetController::class]);
+Route::resources("/commentaires",[Deliver_CommentairesController::class]);
+Route::resources("/competences",[Deliver_CompetenceController::class]);
+Route::resources("/medias",[Deliver_MediaController::class]);
+Route::resources("/tags",[Deliver_TagController::class]);
+
+});
